5791016d17648b0720b006870000294273f2a7d5
AvdhootJadhav <68023630+AvdhootJadhav@users.noreply.github.com>
2021-11-03 20:41:31 +0530

    weighted graph

---
+++ weightedgraph.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<pair<int,int>> E[1000];
+void weightgraph(){
+	int n,m;
+	cin>>n>>m;
+	for(auto i=1;i<=m;++i){
+		int a,b,c;
+		cin>>a>>b>>c;
+		E[a].push_back(make_pair(b,c));
+		E[b].push_back(make_pair(a,c));
+		}
+	for(auto i=1;i<=n;++i){
+		cout<<i<<" ";
+		for(auto x:E[i])cout<<x.first<<" "<<x.second<<" ";
+		cout<<"\n";
+		}
+}
+
+int main(){
+	weightgraph();
+}
